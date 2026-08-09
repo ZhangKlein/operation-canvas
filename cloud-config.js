f66d5d87635e08351f8a8638ab51250eb4ef8717
ZhangKlein <409138851@qq.com>
2026-08-09 23:43:03 +0800

    Add files via upload

---
+++ cloud-config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Public browser configuration only. Never place secret/service-role keys here.
+window.OPERATION_CANVAS_CLOUD_CONFIG = Object.freeze({
+  url: '',
+  anonKey: ''
+});
+
