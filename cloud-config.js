011dabcfe3c3ab49c1db287f5ac208a249fa26c8
ZhangKlein <409138851@qq.com>
2026-08-09 23:44:19 +0800

    Add files via upload

--- cloud-config.js
+++ cloud-config.js
@@ -1,8 +1,5 @@
 'use strict';
-
-// Public browser configuration only. Never place secret/service-role keys here.
 window.OPERATION_CANVAS_CLOUD_CONFIG = Object.freeze({
-  url: '',
-  anonKey: ''
+  "url": "https://woccfvjrrxlnpgjepmei.supabase.co",
+  "anonKey": "sb_publishable_ilEum9T4o0MndEN7ygNCCw_34A7YeVY"
 });
-
